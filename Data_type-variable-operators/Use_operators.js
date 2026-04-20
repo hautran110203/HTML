9673cd1fef7e312f2b0afaf6b49fe627a2747f13
hau110203 <hau.trung110203@gmail.com>
2026-04-20 15:10:21 +0700

    Add: Variable and operation

---
+++ Data_type-variable-operators/Use_operators.js
@@ -0,0 +1,9 @@
+let inputWidth = prompt("Enter the width");
+let inputHeight = prompt("Enter the height");
+
+let width1 = parseInt(inputWidth)
+let height1 = parseInt(inputHeight)
+
+let area1 = width1 * height1
+document.write("Area = "+ area1)
+
